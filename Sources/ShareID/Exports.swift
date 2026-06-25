cc1f3fab35d8d116c2a99520c395d390b7525a6b
Florian DERONE <146930915+florianderone@users.noreply.github.com>
2026-06-25 14:50:12 +0200

    Clean up comments in Exports.swift
    Remove comments explaining the re-export of the binary module.

--- Sources/ShareID/Exports.swift
+++ Sources/ShareID/Exports.swift
@@ -1,4 +1,1 @@
-// Ré-exporte le module binaire pour que le client puisse écrire `import ShareID`.
-// Son code existant `import shareid_sdk_ios` (identique à CocoaPods) continue aussi de fonctionner :
-// dans les deux cas, ajouter le produit "ShareID" à la cible lie également OpenSSL.
 @_exported import shareid_sdk_ios
